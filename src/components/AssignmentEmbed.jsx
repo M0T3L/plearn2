e8e99441739ae23cf54ffe93d4ea141c612a1f7d
Yavuz Burak Kutluca <104972867+M0T3L@users.noreply.github.com>
2025-05-17 15:24:36 +0300

    Create AssignmentEmbed.jsx

---
+++ src/components/AssignmentEmbed.jsx
@@ -0,0 +1,14 @@
+export default function AssignmentEmbed() {
+  return (
+    <div className="p-4 flex justify-center">
+      <iframe
+        src="https://phet.colorado.edu/sims/html/circuit-construction-kit-dc/latest/circuit-construction-kit-dc_tr.html"
+        width="800"
+        height="600"
+        allowFullScreen
+        title="Basit Elektrik Devresi"
+        className="border shadow-lg"
+      ></iframe>
+    </div>
+  );
+}
