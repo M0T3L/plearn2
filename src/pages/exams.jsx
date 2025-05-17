4e50a8377abf02fd823fc33113f95d25269af288
Yavuz Burak Kutluca <104972867+M0T3L@users.noreply.github.com>
2025-05-17 15:14:31 +0300

    Create exams.jsx

---
+++ src/pages/exams.jsx
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar';
+import QuizEmbed from '../components/QuizEmbed';
+
+export default function Exams() {
+  return (
+    <>
+      <Navbar />
+      <QuizEmbed />
+    </>
+  );
+}
